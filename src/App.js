93321e378e85ba98bb3a1297c1a01774d85f8ba2
Dani Marín <danii04.mb@gmail.com>
2023-01-07 15:31:26 +0100

    adding handleClick and handleClickReset

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import { useState } from 'react';
 const App = (props) => {
   const [counter, setCounter] = useState(0)
 
+	const handleClick = () => {
+		setCounter(counter + 1)
+	}
 
+	const handleClickReset = () => {
+		setCounter(0)
+	}
 
   return (
     <div>
 			<p>El valor del contador es:</p>
       <h1>{counter}</h1>
-			<button onClick={() => {
-				// setCounter(counter + 1)
-				setCounter(prevCounter => {
-					return prevCounter + 1
-				})
-			}}>
+			<button onClick={handleClick}>
 				Incrementar
-				</button>
+			</button>
+			<button onClick={handleClickReset}>
+				Reset
+			</button>
     </div>
   )
 }
