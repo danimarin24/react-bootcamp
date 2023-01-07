175bf53b1ea8cc638ca85514f9c0f9027d1f46f2
Dani Marín <danii04.mb@gmail.com>
2023-01-07 15:27:42 +0100

    add useState in App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 
-// Adding new components
-const Title = ({course}) => <h1>{course}</h1>
 
-const Paragraph = ({paragraph, exercise}) => <p>{paragraph} {exercise}</p>
+const App = (props) => {
+  const [counter, setCounter] = useState(0)
 
-const Total = ({ex1, ex2, ex3}) => <p>Number of exercises {ex1 + ex2 + ex3}</p>
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
   return (
     <div>
-      <Title course={course} />
-      <Paragraph paragraph={part1} exercise={exercises1} />
-			<Paragraph paragraph={part2} exercise={exercises2} />
-			<Paragraph paragraph={part3} exercise={exercises3} />
-
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+			<p>El valor del contador es:</p>
+      <h1>{counter}</h1>
+			<button onClick={() => {
+				// setCounter(counter + 1)
+				setCounter(prevCounter => {
+					return prevCounter + 1
+				})
+			}}>
+				Incrementar
+				</button>
     </div>
   )
 }
